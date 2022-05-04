ae798fef0b93f660b0e5d5b775631a3771272e02
brigitteJessica <nkamdumnina@yahoo.fr>
2022-05-04 14:45:13 +0200

    Update streamlit_app.py
    update menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Frühstücksfavoriten')
+streamlit.text('🥣 Omega 3 & Heidelbeer-Haferflocken')
+streamlit.text('🥗 Smoothie mit Grünkohl, Spinat und Rucola')
+streamlit.text('🐔 Hartgekochtes Freilandei')
+streamlit.text('🥑🍞 Avocado-Toast')
