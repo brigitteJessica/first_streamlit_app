95528cb9b7b80480a3f86b84483ba6c59c4de64e
brigitteJessica <nkamdumnina@yahoo.fr>
2022-05-05 17:26:59 +0200

    Update streamlit_app.py
    get

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import requests
+import pandas
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -10,7 +12,6 @@ streamlit.text('🥑🍞 Avocado-Toast')
 
 streamlit.header('🍌🥭 Bau deinen eigenen Frucht-Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -23,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
