1ae972e8cdaf2cc9aba15c5bc927ac297be5996c
brigitteJessica <nkamdumnina@yahoo.fr>
2022-05-05 17:24:31 +0200

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,5 @@ streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.text(fruityvice_response.json())
