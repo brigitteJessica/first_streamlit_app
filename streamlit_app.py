6a6ddbf5e7390ab39efa3266699c8d1e71b69d5f
brigitteJessica <nkamdumnina@yahoo.fr>
2022-05-06 13:00:44 +0200

    Update streamlit_app.py
    fsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,11 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_add = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+fruit_name = 'banana'
+fruit_add = streamlit.text_input('What fruit would you like to add?', fruit_name)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list where fruit_name like fruit_add ")
+my_cur.execute("SELECT * from fruit_load_list where fruit_name like fruit_name")
 my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 streamlit.write('Thanks for adding ', fruit_add)
